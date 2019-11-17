c0cde68537e2c0c2418873b00d7564749da0c44d
RundesBalli <rundesballi@rundesballi.com>
2019-11-18 00:03:12 +0100

    Sitzungsdateien hinzugefügt.

---
+++ public/inc/cookie.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * cookie.php
+ * 
+ * Prüft ob ein gültiger Cookie gesetzt ist.
+ */
+
+if(isset($_COOKIE['stats']) AND !empty($_COOKIE['stats'])) {
+  /**
+   * Cookieinhalt entschärfen und prüfen ob Inhalt ein sha256-Hash ist.
+   */
+  $sessionhash = defuse($_COOKIE['stats']);
+  if(preg_match('/[a-f0-9]{64}/i', $sessionhash, $match) === 1) {
+    /**
+     * Abfrage in der Datenbank, ob eine Sitzung mit diesem Hash existiert.
+     */
+    $result = mysqli_query($dbl, "SELECT `accounts`.`username` FROM `sessions` JOIN `accounts` WHERE `hash`='".$match[0]."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+    if(mysqli_num_rows($result) == 1) {
+      /**
+       * Wenn eine Sitzung existiert wird der letzte Nutzungszeitpunkt aktualisiert und der Username in die Variable $username geladen.
+       */
+      mysqli_query($dbl, "UPDATE `sessions` SET `lastActivity`=CURRENT_TIMESTAMP WHERE `hash`='".$match[0]."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+      setcookie('stats', $match[0], time()+(6*7*86400));
+      $username = mysqli_fetch_array($result)['username'];
+    } else {
+      /**
+       * Wenn keine Sitzung mit dem übergebenen Hash existiert wird der User ausgeloggt.
+       */
+      header("Location: /logout");
+      die();
+    }
+  } else {
+    /**
+     * Wenn kein gültiger sha256 Hash übergeben wurde wird der User ausgeloggt.
+     */
+    header("Location: /logout");
+    die();
+  }
+} else {
+  /**
+   * Wenn kein oder ein leerer Cookie übergeben wurde wird auf die Loginseite weitergeleitet.
+   */
+  header("Location: /login");
+  die();
+}
+?>
---
+++ public/inc/login.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * login.php
+ * 
+ * Seite zum Einloggen.
+ */
+$title = "Login";
+
+/**
+ * Kein Cookie gesetzt oder Cookie leer und Formular nicht übergeben.
+ */
+if((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND !isset($_POST['submit'])) {
+  $content.= "<h1>Login</h1>".PHP_EOL;
+  /**
+   * Cookiewarnung
+   */
+  $content.= "<div class='infobot'>Ab diesem Punkt werden Cookies verwendet! Mit dem Fortfahren stimmst du dem zu!</div>".PHP_EOL.
+  "</div>".PHP_EOL;
+  /**
+   * Loginformular
+   */
+  $content.= "<form action='/login' method='post'>".PHP_EOL;
+  $content.= "<div class='row hover bordered'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-3'>Name</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-8 col-l-9 col-xl-9'><input type='text' name='username' placeholder='Name' autofocus tabindex='1'></div>".PHP_EOL.
+  "</div>".PHP_EOL;
+  $content.= "<div class='row hover bordered'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-3'>Passwort</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-8 col-l-9 col-xl-9'><input type='password' name='password' placeholder='Passwort' tabindex='2'></div>".PHP_EOL.
+  "</div>".PHP_EOL;
+  $content.= "<div class='row hover bordered'>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-4 col-l-3 col-xl-3'>Einloggen</div>".PHP_EOL.
+  "<div class='col-x-12 col-s-12 col-m-8 col-l-9 col-xl-9'><input type='submit' name='submit' value='Einloggen' tabindex='3'></div>".PHP_EOL.
+  "</div>".PHP_EOL;
+  $content.= "</form>".PHP_EOL;
+} elseif((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND isset($_POST['submit'])) {
+  /**
+   * Kein Cookie gesetzt oder Cookie leer und Formular wurde übergeben.
+   */
+  /**
+   * Entschärfen der Usereingaben.
+   */
+  $username = defuse($_POST['username']);
+  /**
+   * Abfragen ob eine Übereinstimmung in der Datenbank vorliegt.
+   */
+  $result = mysqli_query($dbl, "SELECT * FROM `accounts` WHERE `username`='".$username."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+  if(mysqli_num_rows($result) == 1) {
+    /**
+     * Wenn der User existiert, muss der Passworthash validiert werden.
+     */
+    $row = mysqli_fetch_array($result);
+    if(password_verify($_POST['password'].$row['salt'], $row['password'])) {
+      /**
+       * Wenn das Passwort verifiziert werden konnte wird eine Sitzung generiert und im Cookie gespeichert.
+       * Danach erfolg eine Weiterleitung zur Statistik-Seite.
+       */
+      $sessionhash = hash('sha256', time().$_SERVER['REMOTE_ADDR'].rand(10000,99999));
+      mysqli_query($dbl, "INSERT INTO `sessions` (`userid`, `hash`) VALUES ('".$row['id']."', '".$sessionhash."')") OR DIE(MYSQLI_ERROR($dbl));
+      mysqli_query($dbl, "UPDATE `accounts` SET `lastActivity`=NOW() WHERE `id`='".$row['id']."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+      setcookie('stats', $sessionhash, time()+(6*7*86400));
+      header("Location: /stats");
+      die();
+    } else {
+      /**
+       * Wenn das Passwort nicht verifiziert werden konnte wird HTTP403 zurückgegeben und eine Fehlermeldung wird ausgegeben.
+       */
+      http_response_code(403);
+      $content.= "<h1>Login gescheitert</h1>".PHP_EOL;
+      $content.= "<div class='row'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12 warn bold'>Die Zugangsdaten sind falsch.</div>".PHP_EOL.
+      "</div>".PHP_EOL;
+      $content.= "<div class='row'>".PHP_EOL.
+      "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'><a href='/login'>Erneut versuchen</a></div>".PHP_EOL.
+      "</div>".PHP_EOL;
+    }
+  } else {
+    /**
+     * Wenn keine Übereinstimmung vorliegt, dann wird HTTP403 zurückgegeben und eine Fehlermeldung wird ausgegeben.
+     */
+    http_response_code(403);
+    $content.= "<h1>Login gescheitert</h1>".PHP_EOL;
+    $content.= "<div class='row'>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12 warn bold'>Die Zugangsdaten sind falsch.</div>".PHP_EOL.
+    "</div>".PHP_EOL;
+    $content.= "<div class='row'>".PHP_EOL.
+    "<div class='col-x-12 col-s-12 col-m-12 col-l-12 col-xl-12'><a href='/login'>Erneut versuchen</a></div>".PHP_EOL.
+    "</div>".PHP_EOL;
+  }
+} else {
+  /**
+   * Wenn bereits ein Cookie gesetzt ist wird auf die Statistik Seite weitergeleitet.
+   */
+  header("Location: /stats");
+  die();
+}
+?>
