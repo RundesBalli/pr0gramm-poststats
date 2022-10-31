bccfecc737a7986dc1f41f8677183068709da104
RundesBalli <GitHub@RundesBalli.com>
2022-10-31 09:47:22 +0100

    Added routing

---
+++ includes/routing/routes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * includes/routing/routes.php
+ * 
+ * Routes
+ * 
+ * @var array
+ */
+$routes = [
+  /**
+   * Error pages
+   */
+  '404' => 'errors'.DIRECTORY_SEPARATOR.'404.php',
+  '403' => 'errors'.DIRECTORY_SEPARATOR.'403.php',
+
+  /**
+   * Authentication and session
+   */
+  'login' => 'login'.DIRECTORY_SEPARATOR.'redirect.php',
+  'logout' => 'login'.DIRECTORY_SEPARATOR.'logout.php',
+
+  /**
+   * Statistics
+   */
+  'stats' => 'stats.php'
+];
+?>
