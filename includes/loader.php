7e271a45b62d20d17382b23ca1c473bf2c07827e
RundesBalli <GitHub@RundesBalli.com>
2022-10-31 09:51:32 +0100

    Added output function

--- includes/loader.php
+++ includes/loader.php
@@ -10,4 +10,5 @@
  */
 require_once(__DIR__.DIRECTORY_SEPARATOR.'constants.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'configCheck.php');
+require_once(__DIR__.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'output.php');
 ?>
