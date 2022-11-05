14a3c2f188242971ed6beba205f930a88a8e1ed0
RundesBalli <GitHub@RundesBalli.com>
2022-11-05 12:31:52 +0100

    Added files to the cliScripts loader

--- cliScripts/loader.php
+++ cliScripts/loader.php
@@ -11,4 +11,10 @@
 require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'config.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'constants.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'configCheck.php');
+
+/**
+ * Database connection and functions
+ */
+require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'sql.php');
+require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'includes'.DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'defuse.php');
 ?>
