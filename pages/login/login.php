d64360d722cc183bdcedfbe732f5cebf4c792673
RundesBalli <GitHub@RundesBalli.com>
2022-11-05 09:49:57 +0100

    Moved files and adjusted the login

--- pages/login/login.php
+++ pages/login/login.php
@@ -2,21 +2,17 @@
 /**
  * login.php
  * 
- * Seite zum Einloggen.
+ * Login page
  */
 $title = "Login";
 
 /**
- * Kein Cookie gesetzt oder Cookie leer und Formular nicht übergeben.
+ * No cookie set or cookie is empty and form not submitted.
  */
 if((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND !isset($_POST['submit'])) {
   $content.= "<h1>Login</h1>";
   /**
-   * Cookiewarnung
-   */
-  $content.= "<div class='infobox'>Ab diesem Punkt werden Cookies verwendet! Mit dem Fortfahren stimmst du dem zu!</div>";
-  /**
-   * Loginformular
+   * Login form
    */
   $content.= "<form action='/login' method='post'>";
   $content.= "<div class='row hover bordered'>".
@@ -34,34 +30,36 @@ if((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND !isset($_POST['su
   $content.= "</form>";
 } elseif((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND isset($_POST['submit'])) {
   /**
-   * Kein Cookie gesetzt oder Cookie leer und Formular wurde übergeben.
-   */
-  /**
-   * Entschärfen der Usereingaben.
+   * Defusing the user input
    */
   $username = defuse($_POST['username']);
   /**
-   * Abfragen ob eine Übereinstimmung in der Datenbank vorliegt.
+   * Check if there is an account with that username.
    */
   $result = mysqli_query($dbl, "SELECT * FROM `accounts` WHERE `username`='".$username."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
   if(mysqli_num_rows($result) == 1) {
     /**
-     * Wenn der User existiert, muss der Passworthash validiert werden.
+     * If the user exists, the password has to be verified.
      */
     $row = mysqli_fetch_array($result);
     if(password_verify($_POST['password'].$row['salt'], $row['password'])) {
       /**
-       * Wenn das Passwort verifiziert werden konnte wird eine Sitzung generiert und im Cookie gespeichert.
-       * Danach erfolg eine Weiterleitung zur Statistik-Seite.
+       * If the password is correct, the session will be created and the user will be redirected to the stats page.
        */
-      $sessionhash = hash('sha256', time().$_SERVER['REMOTE_ADDR'].rand(10000,99999));
-      mysqli_query($dbl, "INSERT INTO `sessions` (`userid`, `hash`) VALUES ('".$row['id']."', '".$sessionhash."')") OR DIE(MYSQLI_ERROR($dbl));
-      setcookie('stats', $sessionhash, time()+(6*7*86400));
-      header("Location: /stats");
-      die();
+      $hash = sessions::generateHash();
+
+      if(sessions::createSession($row['id'], $hash)) {
+        setcookie('stats', $hash, time()+(8*7*86400), NULL, NULL, TRUE, TRUE);
+        header('Location: /stats');
+        die();
+      } else {
+        setcookie('stats', NULL, 0, NULL, NULL, TRUE, TRUE);
+        header('Location: /login');
+        die();
+      }
     } else {
       /**
-       * Wenn das Passwort nicht verifiziert werden konnte wird HTTP403 zurückgegeben und eine Fehlermeldung wird ausgegeben.
+       * If the password could not be verified HTTP403 is returned and an error message is displayed.
        */
       http_response_code(403);
       $content.= "<h1>Login gescheitert</h1>";
@@ -74,7 +72,7 @@ if((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND !isset($_POST['su
     }
   } else {
     /**
-     * Wenn keine Übereinstimmung vorliegt, dann wird HTTP403 zurückgegeben und eine Fehlermeldung wird ausgegeben.
+     * If there is no match, then HTTP403 is returned and an error message is displayed.
      */
     http_response_code(403);
     $content.= "<h1>Login gescheitert</h1>";
@@ -87,7 +85,7 @@ if((!isset($_COOKIE['stats']) OR empty($_COOKIE['stats'])) AND !isset($_POST['su
   }
 } else {
   /**
-   * Wenn bereits ein Cookie gesetzt ist wird auf die Statistik Seite weitergeleitet.
+   * If a cookie is already set, the user will be redirected to the statistics page.
    */
   header("Location: /stats");
   die();
