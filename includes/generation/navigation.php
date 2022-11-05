c243c06f91f3460b420e1611c4ed3cd3959aace3
RundesBalli <GitHub@RundesBalli.com>
2022-11-05 11:38:19 +0100

    Typo

--- includes/generation/navigation.php
+++ includes/generation/navigation.php
@@ -12,7 +12,7 @@ $nav = "";
  * Check if the user is logged in.
  */
 if(!empty($loginNav) AND $loginNav === TRUE) {
-  $nav.= "<div id='toggleElement'><a id='toggle'>&#x2630; ".$lang['nav']['toggle']."</a></div>";
+  $nav.= "<div id='toggleElement'><a id='toggle'>&#x2630; Menü</a></div>";
   $nav.= "<div><a href='/stats'".($requestedPage == 'stats' ? $a : NULL).">Statistiken</a><a href='/logout'".($requestedPage == 'logout' ? $a : NULL).">Logout</a></div>";
 }
 ?>
