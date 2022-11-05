b63fa0253446688adfda22ca293933be1224f8af
RundesBalli <GitHub@RundesBalli.com>
2022-11-05 09:33:36 +0100

    Removed lang

--- includes/generation/footer.php
+++ includes/generation/footer.php
@@ -4,5 +4,5 @@
  * 
  * Generates footer text
  */
-$footer = "<div>".$lang['footer']['projectBy'].": <a href='https://rundesballi.com' target='_blank' rel='noopener'>RundesBalli</a><a href='https://github.com/RundesBalli/pr0gramm-poststats' target='_blank' rel='noopener'>GitHub</a></div>";
+$footer = "<div>Ein Projekt von: <a href='https://rundesballi.com' target='_blank' rel='noopener'>RundesBalli</a><a href='https://github.com/RundesBalli/pr0gramm-poststats' target='_blank' rel='noopener'>GitHub</a></div>";
 ?>
